b109228a2313be2760afca5b1fce3beaff7c0514
Kurt <wkurtruzzelsapo@gmail.com>
2023-12-19 13:41:41 +0800

    Added a getComputerChoice function
    it will show random choices among the three choices,
    Rock Paper Scissor.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 function playRound(playerSelection, computerSelection) {
 	// your code here!
 }
+function getComputerChoice() {
+	const choices = ['Rock', 'Paper', 'Scissor'];
+	let choice = Math.floor(Math.random() * 3);
 
+	return choices[choice];
+}
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
